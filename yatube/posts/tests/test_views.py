3013a88df9a5e08070daed0003d7f45d49c6613b
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2022-09-09 21:03:49 +0200

    alfa version 1.01 HW04_TESTS

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -37,26 +37,18 @@ class PagesTests(TestCase):
     def test_pages_uses_correct_template(self):
         """URL-адрес использует соответствующий шаблон."""
         templates_pages_names = {
-            'posts/index.html': reverse('posts:index'),
-            'posts/group_list.html': (
-                reverse('posts:group_list', kwargs={'slug': 'test-slug'})
-            ),
-            'posts/profile.html': (
-                reverse('posts:profile', kwargs={'username': 'TestUser'})
-            ),
-            'posts/post_detail.html': (
-                reverse('posts:post_detail', kwargs={
-                    'post_id': f'{self.post.id}'
-                })
-            ),
-            'posts/create_post.html': (
-                reverse('posts:post_edit', kwargs={
-                    'post_id': f'{self.post.id}'
-                })
-            ),
-            'posts/create_post.html': reverse('posts:post_create'),
+            reverse('posts:index'): 'posts/index.html',
+            reverse('posts:group_list', kwargs={'slug': 'test-slug'}): 
+            'posts/group_list.html',
+            reverse('posts:profile', kwargs={'username': 'TestUser'}):
+            'posts/profile.html',
+            reverse('posts:post_detail', kwargs={'post_id': f'{self.post.id}'}):
+            'posts/post_detail.html',
+            reverse('posts:post_edit', kwargs={'post_id': f'{self.post.id}'}):
+            'posts/create_post.html',
+            reverse('posts:post_create'): 'posts/create_post.html',
         }
-        for template, reverse_name in templates_pages_names.items():
+        for reverse_name, template in templates_pages_names.items():
             with self.subTest(reverse_name=reverse_name):
                 response = self.authorized_client.get(reverse_name)
                 self.assertTemplateUsed(response, template)
