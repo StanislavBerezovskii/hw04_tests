322d6396847f5a944bfdf2dc7561a6adddbedddc
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2022-09-13 14:33:14 +0200

    added custom failure pages

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -137,3 +137,4 @@ LOGIN_REDIRECT_URL = 'posts:index'
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 PER_PAGE = 10
+CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
